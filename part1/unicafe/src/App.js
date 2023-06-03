5fea9ea089b3d7a0dffe2433942826c1a9e00d31
peterHuan <2754741241@qq.com>
2023-06-03 21:08:14 +0800

    add unicafe1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,54 @@
 import { useState } from 'react';
-import Statistics from './Statistics'; 
-import Button from './Button';
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const score = good - bad;
+  const average = total === 0? 0: score / total;
+  const positive = total === 0 ? 0 : good / total * 100; 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text='good' onClick={() => setGood(good + 1)}/>
-      <Button text='neutral' onClick={() => setNeutral(neutral + 1)}/>
-      <Button text='bad' onClick={() => setBad(bad + 1)}/>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <table>
+        <thead>
+          <tr>
+            <td colspan="2">Statics</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+              <td>good</td>
+              <td>{good}</td>
+          </tr>
+          <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+          </tr>
+          <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+          </tr>
+          <tr>
+              <td>total</td>
+              <td>{total}</td>
+          </tr>
+          <tr>
+              <td>average</td>
+              <td>{average}</td>
+          </tr>
+          <tr>
+              <td>positive</td>
+              <td>{positive}%</td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>  
   );
 }
--- part1/unicafe/src/StatisticLine.js
+++
@@ -1,5 +0,0 @@
-function StatisticLine({ text, data }) {
-    return <p>{text}: {data} {text === 'positive' ? '%':''}</p>;
-}
-
-export default StatisticLine;
