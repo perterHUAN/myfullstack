8538652097fe6c9ba09b4571062341ec09fb7271
peterHuan <2754741241@qq.com>
2023-06-03 17:30:14 +0800

    add unicafe1.6-1.7

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = good + -1 * bad;
+  const average = total / 3;
+  const positive = total === 0 ? 0 : good / total * 100; 
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+      <h1>Statics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
+
+    </div>  
+  );
+}
+
+export default App;
